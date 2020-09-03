efdf0930bc9bdeef55e628fbebfba753d72300d9
ST17610 <70467512+ST17610@users.noreply.github.com>
2020-09-03 12:44:02 +1200

    Update Menu.py
    Dif layout fix

--- Menu.py
+++ Menu.py
@@ -5,11 +5,13 @@ from tkinter import ttk
 class BaseWindow(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
+        main_frame = tk.Frame(self)
+        main_frame.grid(row = 0,column= 0)
         base = tk.Frame(self)
-        
-        base.pack(side="top", fill="both", expand=True)
-        base.grid_rowconfigure(0, weight=1)
-        base.grid_columnconfigure(0, weight=1)
+        #base.pack(side="top", fill="both", expand=True)
+        base.grid(row = 0,column= 1,sticky="nsew")
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
         self.pages = {}
         
         for P in (Food, Drink, Dessert, Cart):
@@ -35,15 +37,17 @@ class BaseWindow(tk.Tk):
         button4 = ttk.Button(self, text="Cart",
                             command=lambda: self.show_page(Cart))
 
-        button1.pack()
-        button2.pack()
-        button3.pack()
+        button1.grid(in_=main_frame,row=0, column=0, sticky="nsew")
+        button2.grid(in_=main_frame,row=1, column=0, sticky="nsew")
+        button3.grid(in_=main_frame,row=2, column=0, sticky="nsew")
+        button4.grid(in_=main_frame,row=3, column=0, sticky="nsew")
+    """"
+        main_frame.button1.pack()
+        main_frame.button2.pack()
+        main_frame.button3.pack()
         
         """
-        button1.grid(row=0, column=0, sticky="nsew")
-        button2.grid(row=1, column=0, sticky="nsew")
-        button3.grid(row=2, column=0, sticky="nsew")
-        button4.grid(row=3, column=0, sticky="nsew")"""
+        
       
     def show_page(self, page_num):
         page = self.pages[page_num]
