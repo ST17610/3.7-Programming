00b262a9ebdc0e451857c30e9c86738641a75a6f
ST17610 <ST17610@howick.school.nz>
2020-09-09 23:36:04 +1200

    Style and started checkout button almost at MVP

--- Menu.py
+++ Menu.py
@@ -23,7 +23,7 @@ class BaseWindow(tk.Tk):
             self.page.grid(row=0, column=0, sticky="nsew")
 
         self.show_page(Food)
-        print(self.pages)
+        #print(self.pages)
         
     def show_page(self, page_num):
         self.page = self.pages[page_num]
@@ -61,17 +61,25 @@ class BasePage():
         parent.label = tk.Label(parent.menubox, text=name)
         #parent.label.grid(column=1, row=0, pady=10, padx=10)
         
-        parent.button1 = tk.Button(parent.nav, text="Food",
-                            command=lambda: BaseWindow.show_page(Food))
+        parent.button1 = tk.Button(parent.nav, text="Food",bg="#F0EFF4",
+                                   highlightthickness=0,
+                                   pady=20,
+                                   command=lambda: BaseWindow.show_page(Food))
 
-        parent.button2 = tk.Button(parent.nav, text="Drink",
-                            command=lambda: BaseWindow.show_page(Drink))
+        parent.button2 = tk.Button(parent.nav, text="Drink",bg="#F0EFF4",
+                                   highlightthickness=0,
+                                   pady=20,
+                                   command=lambda: BaseWindow.show_page(Drink))
 
-        parent.button3 = tk.Button(parent.nav, text="Dessert",
-                            command=lambda: BaseWindow.show_page(Dessert))
+        parent.button3 = tk.Button(parent.nav, text="Dessert",bg="#F0EFF4",
+                                   highlightthickness=0,
+                                   pady=20,
+                                   command=lambda: BaseWindow.show_page(Dessert))
 
-        parent.button4 = tk.Button(parent.nav, text="Cart",
-                            command=lambda: BaseWindow.show_page(Cart))
+        parent.button4 = tk.Button(parent.nav, text="Cart",bg="#F0EFF4",
+                                   highlightthickness=0,
+                                   pady=20,
+                                   command=lambda: BaseWindow.show_page(Cart))
         
         parent.button1.grid(row=0, column=0, sticky="nsew")
         parent.button2.grid(row=1, column=0, sticky="nsew")
@@ -85,10 +93,10 @@ class BasePage():
                 parent.menubox.columnconfigure(0, weight=1)
                 parent.menubox.rowconfigure(i, weight=1)
                 parent.box.append(tk.Button(parent.menubox,
-                                          bg="#F0EFF4",
-                                          highlightthickness=0,
-                                          text=food,
-                                          command=lambda food=food :BaseWindow.create_quantity(food)))
+                                            bg="#F0EFF4",
+                                            highlightthickness=0,
+                                            text=food,
+                                            command=lambda food=food :BaseWindow.create_quantity(food)))
                 parent.box[i].grid(row=i, column=0, sticky="nsew")
                 i+=1
         
@@ -125,8 +133,12 @@ class Quantity(tk.Frame):
         self.menubox.rowconfigure(4, weight=1)
         
         self.num = 0
-        self.food_label = tk.Label(self.menubox, text=food)
-        self.num_label = tk.Label(self.menubox, text=self.num)
+        self.food_label = tk.Label(self.menubox, text=food,
+                                   bg="#F0EFF4",
+                                   highlightthickness=0,)
+        self.num_label = tk.Label(self.menubox, text=self.num,
+                                  bg="#F0EFF4",
+                                  highlightthickness=0,)
         def int_button_control(self, state):
             if state == '+':
                 self.num += 1 
@@ -135,9 +147,15 @@ class Quantity(tk.Frame):
             self.num_label.configure(text=self.num)
                 
     
-        self.up = tk.Button(self.menubox, text="↑",command=lambda:int_button_control(self,'+'))
-        self.down = tk.Button(self.menubox, text="↓", command=lambda:int_button_control(self, '-'))
-        self.enter = tk.Button(self.menubox, text="Enter", command=lambda:Cart.add_cart(BaseWindow.pages[Cart], food, self.num))
+        self.up = tk.Button(self.menubox, text="↑",bg="#F0EFF4",
+                            highlightthickness=0,
+                            command=lambda:int_button_control(self,'+'))
+        self.down = tk.Button(self.menubox, text="↓",bg="#F0EFF4",
+                              highlightthickness=0,
+                              command=lambda:int_button_control(self, '-'))
+        self.enter = tk.Button(self.menubox, text="Enter",bg="#F0EFF4",
+                               highlightthickness=0,
+                               command=lambda:Cart.add_cart(BaseWindow.pages[Cart], food, self.num))
         
         self.food_label.grid(row=0, column=0, sticky="nsew")
         self.up.grid(row=1, column=0, sticky="nsew")
@@ -150,24 +168,31 @@ class Cart(tk.Frame):
     def __init__(self, parent, BaseWindow):
         tk.Frame.__init__(self,parent)
         BasePage(self, BaseWindow, "Cart")
+        self.checkout = tk.Button(self.footer,
+                                  bg="#F0EFF4",
+                                  highlightthickness=0,
+                                  text="Checkout",
+                                  command=lambda:map(self.cart.print_info(), self.cart))
+        self.checkout.grid(row=0 ,column=0, sticky="nsew")
         self.cart = []
         self.loc = 0
         
     def add_cart(self, food, num):
+        self.Items(food, num)
+        self.menubox.columnconfigure(0, weight=1)
+        self.menubox.rowconfigure(self.loc, weight=1)
+        self.box.append(tk.Button(self.menubox,
+                                  bg="#F0EFF4",
+                                  highlightthickness=0,
+                                  text=f"{food} Quantity : {num}"))
+                                    #command=lambda food=food :BaseWindow.create_quantity(food)))
+        self.box[self.loc].grid(row=self.loc, column=0, sticky="nsew")
+        print(self.cart)
+        self.loc+=1
+    
+    def Items(self, food, num):
         for i in range(num):
-            #self.cart.append(Items(food))
-            self.menubox.columnconfigure(0, weight=1)
-            self.menubox.rowconfigure(self.loc, weight=1)
-            self.box.append(tk.Button(self.menubox,
-                                        bg="#F0EFF4",
-                                        highlightthickness=0,
-                                        text=f"{food}"))
-                                        #command=lambda food=food :BaseWindow.create_quantity(food)))
-            self.box[self.loc].grid(row=self.loc, column=0, sticky="nsew")
-            print(self.box)
-            self.loc+=1
-            
-            
+            self.cart.append(Items(food))
        
             
         
@@ -176,7 +201,13 @@ class Items():
     item_data = json.load(file)
     file.close()
     def __init__(self, item):
-        pass
+        for name in Items.item_data.keys():
+            if item in Items.item_data[name].keys():
+                self.info = Items.item_data[name][item]
+                
+    def print_info(self):
+        print(self.info)
+        
 
         
 if __name__ == "__main__":
